161596227e3b26a4b8880e905fdf724f3d2e2c46
tuanvu <tuan.vu1802@gmail.com>
2019-08-22 20:51:32 +0700

    Deploy App to Heroku

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ Db();
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/tasks", require("./routes/api/tasks"));
 
-// // Server in production setup
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   // Get all routers
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
-// }
+// Server in production setup
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  // Get all routers
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Lauch Server
 PORT = process.env.PORT || 5000;
